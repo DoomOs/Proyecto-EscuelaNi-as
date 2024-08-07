e64abf15103bbdd0d45de76ce5b4de35d8189086
DoomOs <jancarlososa@gmail.com>
2024-08-07 09:37:44 -0600

    feat: :tada: Base, creacion de app

--- Escuela/settings.py
+++ Escuela/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "Escuela.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_escuelita',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
