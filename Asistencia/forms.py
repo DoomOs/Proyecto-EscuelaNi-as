bc31a505a6aca474b4558ddd944de3521361046d
DoomOs <jancarlososa@gmail.com>
2024-10-03 12:58:37 -0600

    docs: :memo: Creacion de docstring en views, models, forms del modulo Asistencia

--- Asistencia/forms.py
+++ Asistencia/forms.py
@@ -2,6 +2,17 @@ from django import forms
 from .models import Asistencia
 
 class AsistenciaForm(forms.ModelForm):
+    """
+        Formulario para registrar la asistencia de una alumna en un día específico.
+
+    Atributos:
+        Meta (class): Clase interna que define la configuración del formulario.
+            model (Model): El modelo asociado al formulario (Asistencia).
+            fields (list): Lista de campos que se incluirán en el formulario, en este caso solo 'presente'.
+            widgets (dict): Diccionario que define la representación de los campos del formulario; 
+            el campo 'presente' se representará como un checkbox con clase 'form-check-input'.
+
+    """
     class Meta:
         model = Asistencia
         fields = ['presente']
--- Asistencia/models.py
+++ Asistencia/models.py
@@ -4,6 +4,19 @@ from Asignacion_Ciclo.models import AsignacionCiclo
 
 # Create your models here.
 class Asistencia(models.Model):
+    """
+        Modelo que representa la asistencia de una alumna en un día específico.
+
+    Atributos:
+        fecha (DateField): Fecha de la asistencia.
+        presente (BooleanField): Indica si la alumna estuvo presente (True) o ausente (False).
+        asignacion_ciclo (ForeignKey): Relación con el modelo AsignacionCiclo, representando la asignación 
+        del ciclo escolar de la alumna a la que se refiere esta asistencia.
+
+    Métodos:
+        __str__(): Retorna una representación en cadena de la fecha de asistencia.
+
+    """
     fecha = models.DateField()
     presente = models.BooleanField(default=False)
     asignacion_ciclo = models.ForeignKey(AsignacionCiclo, on_delete=models.CASCADE)
