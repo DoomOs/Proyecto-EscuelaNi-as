cc51c9d137e34f6a0b4d846d7e6de80de06f9b50
DoomOs <jancarlososa@gmail.com>
2024-10-03 13:36:25 -0600

    docs: :memo: creacion de docstring para views, models y forms del modulo Persona

--- Persona/views.py
+++ Persona/views.py
@@ -14,6 +14,22 @@ from django.db.models import Max, Q
 from django.contrib.auth.decorators import login_required
 
 def persona_create_view(request):
+    """
+        Maneja la creación de una nueva persona y redirige a la vista de creación de alumna.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP que contiene los datos del formulario de persona.
+
+    Retorna:
+        HttpResponse: Redirige a la vista de creación de alumna con el ID de la persona creada, o renderiza el formulario de persona si hay errores.
+
+    Proceso:
+        - Si el método de la solicitud es POST, se valida el formulario de Persona.
+        - Si el formulario es válido, se guarda la persona y se redirige a la vista de creación de alumna.
+        - Si el formulario no es válido, se renderiza de nuevo el formulario de Persona.
+
+    """
+    
     if request.method == 'POST':
         form = PersonaForm(request.POST)
         if form.is_valid():
@@ -25,6 +41,24 @@ def persona_create_view(request):
     return render(request, 'persona_form.html', {'form': form})
 
 def alumna_create_view(request):
+    
+    """
+        Maneja la creación de una nueva alumna asociada a una persona existente.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP que contiene los datos del formulario de alumna.
+
+    Retorna:
+        HttpResponse: Redirige a la vista de creación de contacto con el ID de la alumna creada, o renderiza el formulario de alumna si hay errores.
+
+    Proceso:
+        - Obtiene el ID de la persona desde la URL.
+        - Si el método de la solicitud es POST, se valida el formulario de Alumna.
+        - Si el formulario es válido, se guarda la alumna y se redirige a la vista de creación de contacto.
+        - Si el formulario no es válido, se renderiza de nuevo el formulario de Alumna con el ID de la persona como valor inicial.
+
+    """
+    
     persona_id = request.GET.get('persona_id')
 
     if request.method == 'POST':
@@ -41,6 +75,23 @@ def alumna_create_view(request):
 
 
 def contacto_create_view(request, alumna_id):
+    """
+        Maneja la creación de un nuevo contacto asociado a una alumna específica.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP que contiene los datos del formulario de contacto.
+        alumna_id (int): El ID de la alumna a la que se asociará el nuevo contacto.
+
+    Retorna:
+        HttpResponse: Renderiza el formulario de contacto con la lista de contactos existentes de la alumna.
+
+    Proceso:
+        - Obtiene la alumna usando el ID proporcionado.
+        - Si el método de la solicitud es POST, se valida el formulario de Contacto.
+        - Si el formulario es válido, se guarda el contacto asociado a la alumna.
+        - Renderiza el formulario de contacto con todos los contactos de la alumna.
+
+    """
     alumno = get_object_or_404(Alumna, id=alumna_id)
 
     if request.method == 'POST':
@@ -64,6 +115,24 @@ def contacto_create_view(request, alumna_id):
     })
 
 def contacto_edit_view(request, contacto_id):
+    """
+        Maneja la edición de un contacto existente.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP que contiene los datos del formulario de contacto.
+        contacto_id (int): El ID del contacto a editar.
+
+    Retorna:
+        HttpResponse: Renderiza el formulario de contacto editado y la lista de contactos de la alumna.
+
+    Proceso:
+        - Obtiene el contacto usando el ID proporcionado.
+        - Si el método de la solicitud es POST, se valida el formulario de Contacto.
+        - Si el formulario es válido, se guarda el contacto editado.
+        - Renderiza el formulario de contacto con la lista de contactos de la alumna.
+
+    """
+    
     contacto = get_object_or_404(Contacto, id=contacto_id)
     
     if request.method == 'POST':
@@ -82,6 +151,22 @@ def contacto_edit_view(request, contacto_id):
     })
 
 def contacto_delete_view(request, contacto_id):
+    """
+        Maneja la eliminación de un contacto existente.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP que contiene la acción de eliminación.
+        contacto_id (int): El ID del contacto a eliminar.
+
+    Retorna:
+        HttpResponse: Redirige a la vista de creación de contacto de la alumna correspondiente.
+
+    Proceso:
+        - Obtiene el contacto usando el ID proporcionado.
+        - Si el método de la solicitud es POST, se elimina el contacto.
+        - Redirige a la vista de creación de contacto de la alumna después de la eliminación.
+
+    """
     contacto = get_object_or_404(Contacto, id=contacto_id)
     if request.method == 'POST':
         alumno_id = contacto.alumna.id
@@ -95,6 +180,22 @@ def contacto_delete_view(request, contacto_id):
 
 @login_required
 def alumna_list_view(request):
+    """
+        Maneja la visualización de la lista de alumnas activas.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP que contiene la información del usuario.
+
+    Retorna:
+        HttpResponse: Renderiza la lista de alumnas activas, organizadas por asignación.
+
+    Proceso:
+        - Obtiene el usuario logueado y su grado asignado.
+        - Filtra las alumnas activas y obtiene su mejor asignación.
+        - Calcula la edad de cada alumna y organiza la lista según su grado.
+        - Renderiza la lista de alumnas en el contexto de la plantilla.
+
+    """
     user = request.user  # Obtener el usuario logueado
     grado_usuario_id = user.id_ciclo  # Obtener el grado asignado al usuario
 
@@ -143,6 +244,20 @@ def alumna_list_view(request):
 
     
 def alumna_inactive_list_view(request):
+    """
+        Maneja la visualización de la lista de alumnas inactivas.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP.
+
+    Retorna:
+        HttpResponse: Renderiza la lista de alumnas inactivas.
+
+    Proceso:
+        - Filtra las alumnas inactivas y calcula su edad y asignación.
+        - Renderiza la lista de alumnas inactivas en el contexto de la plantilla.
+
+    """
     alumnas = Alumna.objects.filter(estado=False)  # Solo alumnas inactivas
     for alumna in alumnas:
         alumna.edad = now().year - alumna.persona.fecha_nacimiento.year
@@ -154,12 +269,44 @@ def alumna_inactive_list_view(request):
     
 
 def desactivar_alumna_view(request, alumna_id):
+    """
+        Maneja la desactivación de una alumna específica.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP.
+        alumna_id (int): El ID de la alumna a desactivar.
+
+    Retorna:
+        HttpResponse: Redirige a la lista de alumnas activas.
+
+    Proceso:
+        - Obtiene la alumna usando el ID proporcionado.
+        - Cambia el estado de la alumna a inactiva y guarda los cambios.
+        - Redirige a la lista de alumnas activas.
+
+    """
     alumna = get_object_or_404(Alumna, id=alumna_id)
     alumna.estado = False
     alumna.save()
     return redirect('alumna-list')  # Redirige al listado de alumnas activas
 
 def activar_alumna_view(request, alumna_id):
+    """
+        Maneja la activación de una alumna específica.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP.
+        alumna_id (int): El ID de la alumna a activar.
+
+    Retorna:
+        HttpResponse: Redirige a la lista de alumnas inactivas.
+
+    Proceso:
+        - Obtiene la alumna usando el ID proporcionado.
+        - Cambia el estado de la alumna a activa y guarda los cambios.
+        - Redirige a la lista de alumnas inactivas.
+
+    """
     alumna = get_object_or_404(Alumna, id=alumna_id)
     alumna.estado = True
     alumna.save()
@@ -168,6 +315,23 @@ def activar_alumna_view(request, alumna_id):
     
 
 def alumna_edit_view(request, alumna_id):
+    """
+        Maneja la edición de los datos de una alumna existente.
+
+    Parámetros:
+        request (HttpRequest): La solicitud HTTP que contiene los datos del formulario de alumna.
+        alumna_id (int): El ID de la alumna a editar.
+
+    Retorna:
+        HttpResponse: Renderiza el formulario de edición de alumna, persona y asignación.
+
+    Proceso:
+        - Obtiene la alumna y su persona asociada usando el ID proporcionado.
+        - Si el método de la solicitud es POST, se validan los formularios de Alumna y Persona.
+        - Si ambos formularios son válidos, se guardan los datos y se maneja la asignación de ciclo.
+        - Renderiza el formulario de edición con los datos existentes si no es una solicitud POST.
+
+    """
     alumna = get_object_or_404(Alumna, id=alumna_id)
     persona = alumna.persona
     
